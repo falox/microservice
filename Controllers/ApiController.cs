c068b7bebb6a5d8f23a932dfcbc2cea9138c4e3a
Alberto Falossi <26248823+falox@users.noreply.github.com>
2020-09-18 22:18:47 +0200

    Initial commit

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace backend.Controllers
+{
+    [Route("api")]
+    public class ApiController : Controller
+    {
+        private readonly ILogger<ApiController> _logger;
+        private static int _count;
+
+        public ApiController(ILogger<ApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("version")]
+        public IActionResult GetVersion()
+        {
+            return Ok("0.0.1");
+        }
+
+        [HttpGet("data")]
+        public IActionResult GetData()
+        {
+            return Json(new { Count = ++_count, Host = System.Net.Dns.GetHostName() });
+        }
+    }
+}
