5c1f4a0be5437aae79e140ad036736a52d48c9fd
Alberto Falossi <26248823+falox@users.noreply.github.com>
2020-09-19 10:42:07 +0200

    Changed port to 5000

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -22,7 +22,7 @@ namespace backend.Controllers
         [HttpGet("version")]
         public IActionResult GetVersion()
         {
-            return Ok("0.0.2");
+            return Ok("0.0.3");
         }
 
         [HttpGet("data")]
